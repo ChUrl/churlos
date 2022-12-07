61d15f1be566cfd94e809159fd410b59c85f85fe
ChUrl <christoph.urlacher@protonmail.com>
2022-12-07 21:36:22 +0100

    Add empty services

---
+++ src/kernel/service/InterruptService.cpp
@@ -0,0 +1,1 @@
+#include "InterruptService.h"
---
+++ src/kernel/service/Service.h
@@ -0,0 +1,12 @@
+#ifndef CHURLOS_SERVICE_H
+#define CHURLOS_SERVICE_H
+
+namespace Kernel {
+
+class Service {
+
+};
+
+}
+
+#endif //CHURLOS_SERVICE_H
