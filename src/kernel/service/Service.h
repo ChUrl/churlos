4838700c8edff4bb855bde54681c9ca01d3ee40c
ChUrl <christoph.urlacher@protonmail.com>
2022-12-08 02:21:36 +0100

    Add missing constructors to Service.h

--- src/kernel/service/Service.h
+++ src/kernel/service/Service.h
@@ -1,10 +1,25 @@
 #ifndef CHURLOS_SERVICE_H
 #define CHURLOS_SERVICE_H
 
+#include <cstdint>
+
 namespace Kernel {
 
 class Service {
+public:
+    Service() = default;
 
+    // TODO: Macros for unmovable, uncopyable
+
+    Service(const Service &copy) = delete;
+
+    Service(Service &&move) = delete;
+
+    Service operator=(const Service &copy) = delete;
+
+    Service &operator=(Service &&move) = delete;
+
+    virtual ~Service() = default;
 };
 
 }
